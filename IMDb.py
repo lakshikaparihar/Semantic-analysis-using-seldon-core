63fffeceb9dc15431ee1e170ad9c2fa01b20affd
Nishkarsh Raj <nishkarshraj000@gmail.com>
2021-03-09 10:05:33 +0530

    Update IMDb.py

--- IMDb.py
+++ IMDb.py
@@ -16,13 +16,12 @@ class IMDb(object):
         predictions = self.loaded_model.predict(tfidf_features)
         predict_prob =self.loaded_model._predict_proba_lr(tfidf_features)
         print("Predicting result .......................")
-        predict = {}
+        predict = dict()
         predict.update({"Sample":X[0]})
         predict.update({"Feedback value":predictions[0]})
         if predictions[0] == "pos":
             predict.update({"Probability":predict_prob[0][1]})
         else :
             predict.update({"Probability":predict_prob[0][0]})
-        print(predict)
         json_object = json.dumps(predict, indent = 4)
-        return json.dumps(json_object)
+        return json_object
