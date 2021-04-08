dd2c8a3b4688385793b5bfd770208fcc00bc3426
NishkarshRaj <nishkarshraj000@gmail.com>
2021-04-08 15:58:14 +0530

    Update

--- mlflow/deploy_model.sh
+++ mlflow/deploy_model.sh
@@ -3,6 +3,6 @@
 echo "Deploying Production model name=MovieModel"
 
 # Set enviorment variable for the tracking URL where the Model Registry is
-export MLFLOW_TRACKING_URI=sqlite:///mlruns.db
+export MLFLOW_TRACKING_URI=mysql+pymysql://nishkarsh:nishkarsh@nishkarsh.cmuzdwd6qin5.ap-south-1.rds.amazonaws.com:3306/nishkarsh
 # Serve the production model from the model registry
-mlflow models serve --model-uri models:/MovieModel/production --no-conda
+mlflow models serve --model-uri models:/MovieModel/production --no-conda -p 3000
